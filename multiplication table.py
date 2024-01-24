dbee924b6625e5fd42c0476efa33231b0ba99ee1
erichestella <erichestella05@gmail.com>
2024-01-24 22:45:21 +0800

    variable for multiplication

--- multiplication table.py
+++ multiplication table.py
@@ -1,2 +1,6 @@
 #adding up some title 
 print('\n1-10 MULTIPLICATION TABLE\n')
+
+#variable for the multiplication number
+digits_numerical = [1,2,3,4,5,6,7,8,9,10]
+digits_integers = [1,2,3,4,5,6,7,8,9,10]
