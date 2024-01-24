118d8c74a934592039e6d2f69d74e8c2fcb0ec05
erichestella <erichestella05@gmail.com>
2024-01-24 22:47:28 +0800

    adding up some function to multiply the two given variables

--- multiplication table.py
+++ multiplication table.py
@@ -1,6 +1,12 @@
 #adding up some title 
 print('\n1-10 MULTIPLICATION TABLE\n')
 
-#variable for the multiplication number
+#variable for the multiplication table
 digits_numerical = [1,2,3,4,5,6,7,8,9,10]
 digits_integers = [1,2,3,4,5,6,7,8,9,10]
+
+#this function is to multiply the two variable given above 
+for t in digits_numerical:
+    for n in digits_integers:
+        print(t * n, end= '\t')
+    print()
